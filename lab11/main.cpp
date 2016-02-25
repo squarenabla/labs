42aa1a2bfcc49a8ac72709cf995dd5c408df1710
squarenabla <square.nabla@gmail.com>
2016-02-26 00:00:44 +0300

    lab 11

---
+++ lab11/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <list>
+
+
+using namespace std;
+
+struct Patient{
+	string firstName = "Bob";
+	string lastName = "Smith";
+	string phoneNum = "88005553535";
+	string address = "Empire State Building";
+	string idNum = "777";
+	string otherShit = "?";
+};
+
+list<Patient> linearList;
+
+// template<typename T> 
+// list<T>& removeElement(){
+	
+// }
+
+int main(){
+
+	return 0;
+}
